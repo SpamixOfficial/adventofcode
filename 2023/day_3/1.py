899e7a4108597dd1669f3b62d6a75d94b7c6917e
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-12-05 21:28:49 +0100

    Update 1.py

--- 2023/day_3/1.py
+++ 2023/day_3/1.py
@@ -1,2 +1,40 @@
 lines = open("input.txt").read().splitlines()
-print(lines)
+height = len(lines)
+width = len(lines[0])
+xindex = 0
+yindex = 0
+cnum = ""
+broken = False
+tempstr = ""
+finalnum = 0
+nums = []
+tempout = ""
+found = False
+for line in lines:
+    for character in line: 
+        if character.isdigit():
+            if not found:
+                for y in range(-1, 2):
+                    if not yindex+y > 139:
+                        for x in range (-1, 2): 
+                            if not xindex+x > 128:
+                                if not lines[yindex+y][xindex+x].isdigit() and lines[yindex+y][xindex+x] != ".":
+                                    tempstr += character
+                                    found = True
+            else:
+                tempstr += character
+        else: 
+            found = False
+            if tempstr != "":
+                nums.append(tempstr)
+                finalnum += int(tempstr)
+                tempstr = ""
+                    
+        xindex += 1
+    xindex = 0
+    yindex += 1
+#tempnum = 0
+#for value in nums:
+#    tempnum += int(value)
+print(finalnum)
+#print(nums)
